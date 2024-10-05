3ab386d49761dd6102aeb4f9074030ec44c08056
Chris Russell <chris@bitsplash.com>
2024-10-05 14:14:51 +0100

    condition expression not working yet and get ticket endpoint

--- src/main.go
+++ src/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 	"os"
@@ -10,38 +15,35 @@ import (
 )
 
 func main() {
-	pgHost := os.Getenv("PG_HOST")
-	if len(pgHost) == 0 {
-		log.Fatal().Msg("PG_HOST env var must be set")
+	dynamoTable := os.Getenv("DYNAMO_TABLE")
+	if len(dynamoTable) == 0 {
+		log.Fatal().Msg("DYNAMO_TABLE env var must be set")
 	}
 
-	username := os.Getenv("PG_USER")
-	if len(username) == 0 {
-		log.Fatal().Msg("PG_USER env var must be set")
+	dynamoHost := os.Getenv("DYNAMO_HOST")
+	if len(dynamoHost) == 0 {
+		log.Fatal().Msg("DYNAMO_HOST env var must be set")
 	}
 
-	password := os.Getenv("PG_PASS")
-	if len(password) == 0 {
-		log.Fatal().Msg("PG_PASS env var must be set")
+	awsConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("eu-west-2"), config.WithCredentialsProvider(credentials.StaticCredentialsProvider{Value: aws.Credentials{
+		AccessKeyID:     "123",
+		SecretAccessKey: "123",
+		SessionToken:    "dummy",
+		Source:          "local dynamo",
+	}}))
+	if err != nil {
+		log.Fatal().Err(err).Msg("unable to load aws config")
 	}
 
-	dbName := os.Getenv("PG_NAME")
-	if len(dbName) == 0 {
-		log.Fatal().Msg("PG_NAME env var must be set")
-	}
-
-	pgTicketOptionStorage := storage.NewPostgresTicketOptionStorage(&storage.PostgresStorageConfig{
-		Host:     pgHost,
-		Username: username,
-		Password: password,
-		DbName:   dbName,
+	dynamoCli := dynamodb.NewFromConfig(awsConfig, func(o *dynamodb.Options) {
+		o.BaseEndpoint = aws.String(dynamoHost)
 	})
 
-	defer pgTicketOptionStorage.Close()
+	dynamoTicketStorage := storage.NewDynamoTicketOptionStorage(dynamoCli, dynamoTable)
 
 	r := gin.Default()
 
-	ticketOptionService := ticketoption.NewDefaultTicketOptionService(pgTicketOptionStorage)
+	ticketOptionService := ticketoption.NewDefaultTicketOptionService(dynamoTicketStorage)
 	ticketOptionController := controller.NewTicketOptionController(ticketOptionService)
 
 	AddTicketOptionsRoutes(r, ticketOptionController)
--- src/storage/dynamo.go
+++ src/storage/dynamo.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -60,8 +61,78 @@ func (d *DynamoTicketOptionStorage) GetTicketOption(ctx context.Context, ticketO
 }
 
 func (d *DynamoTicketOptionStorage) GenerateTickets(ctx context.Context, input *GenerateTicketsInput) (*GenerateTicketsResult, error) {
-	//TODO implement me
-	panic("implement me")
+	purchaseId := uuid.New().String()
+
+	_, err := d.cli.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+		Key: map[string]types.AttributeValue{
+			"pk": &types.AttributeValueMemberS{Value: input.TicketOptionId},
+			"sk": &types.AttributeValueMemberS{Value: "TICKETOPTION"},
+		},
+		TableName:           aws.String(d.tableName),
+		ConditionExpression: aws.String("allocation >= :zeroValue"),
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":zeroValue": &types.AttributeValueMemberN{Value: "0"},
+			":q":         &types.AttributeValueMemberN{Value: strconv.FormatInt(int64(input.Quantity*-1), 10)},
+		},
+		UpdateExpression: aws.String("ADD allocation :q"),
+	})
+	if err != nil {
+		var cExpErr *types.ConditionalCheckFailedException
+		if errors.As(err, &cExpErr) {
+			return nil, ErrTicketOptionAllocationCheckFailed
+		} else {
+			return nil, fmt.Errorf("unable to allocate tickets: %w", err)
+		}
+	}
+
+	_, err = d.cli.PutItem(ctx, &dynamodb.PutItemInput{
+		Item: map[string]types.AttributeValue{
+			"pk":       &types.AttributeValueMemberS{Value: input.TicketOptionId},
+			"sk":       &types.AttributeValueMemberS{Value: "PURCHASE"},
+			"user_id":  &types.AttributeValueMemberS{Value: input.UserId},
+			"quantity": &types.AttributeValueMemberN{Value: strconv.FormatInt(int64(input.Quantity), 10)},
+			"id":       &types.AttributeValueMemberS{Value: purchaseId},
+		},
+		TableName: aws.String(d.tableName),
+	})
+	if err != nil {
+		// TODO roll back allocation change
+		return nil, fmt.Errorf("unable to create purchase: %w", err)
+	}
+
+	var batchTicketWriteRequests []types.WriteRequest
+
+	var ticketIds []string
+	for i := 1; i <= input.Quantity; i++ {
+		ticketId := uuid.New().String()
+		batchTicketWriteRequests = append(batchTicketWriteRequests, types.WriteRequest{
+			PutRequest: &types.PutRequest{
+				Item: map[string]types.AttributeValue{
+					"pk":          &types.AttributeValueMemberS{Value: input.TicketOptionId},
+					"sk":          &types.AttributeValueMemberS{Value: fmt.Sprint("TICKET#", ticketId)},
+					"purchase_id": &types.AttributeValueMemberS{Value: purchaseId},
+					"ticket_id":   &types.AttributeValueMemberS{Value: ticketId},
+				},
+			},
+		})
+
+		ticketIds = append(ticketIds, ticketId)
+	}
+
+	_, err = d.cli.BatchWriteItem(ctx, &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]types.WriteRequest{
+			d.tableName: batchTicketWriteRequests,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to write tickets: %w", err)
+	}
+
+	return &GenerateTicketsResult{
+		PurchaseId: purchaseId,
+		TicketIds:  ticketIds,
+	}, nil
+
 }
 
 func (d *DynamoTicketOptionStorage) Close() {
